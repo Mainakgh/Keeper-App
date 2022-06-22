a1ebc19ec3229e268ac2731325ab1259fdcc40d3
Mainakgh <fcsmainak8@gmail.com>
2022-06-22 12:13:49 +0530

    First Commit

---
+++ build/precache-manifest.50fcebc8d09c998c6286d8d86184b143.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce9e2111d1cfcf19a1152e543458c4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaa1195ee5dceb2a2e56",
+    "url": "/static/js/2.6e44967c.chunk.js"
+  },
+  {
+    "revision": "32f2c65af6b043e4f957",
+    "url": "/static/js/main.03c2feaf.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
